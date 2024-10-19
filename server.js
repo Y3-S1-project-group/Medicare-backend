a12a700d48decda3d7fe87024f5493475082f720
Buddhima <128503846+Zeus4808@users.noreply.github.com>
2024-10-19 13:44:06 +0530

    ok

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import AppoinmentRouter from './routes/AppoinmentRouter.js';
 import staffRouter from "./routes/Staffs.js";
 import ReportRouter from "./routes/reportRouter.js";
 import authRoutes from './routes/auth.js';
-import adminRoutes from './routes/adminRoutes.js';
+// import adminRoutes from './routes/adminRoutes.js';
 
 dotenv.config();
 
@@ -29,7 +29,7 @@ app.use("/api/staff", staffRouter);
 app.use('/Appoint', AppoinmentRouter);
 app.use('/report', ReportRouter);
 app.use('/api/auth', authRoutes);
-app.use('/api/admin', adminRoutes);
+// app.use('/api/admin', adminRoutes);
 
 // Start the server
 app.listen(port, () => {
